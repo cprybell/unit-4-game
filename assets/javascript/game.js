8b9d1a41908413a43a292b21200ab9b40d659b4a
cprybell <cprybell@sbcglobal.net>
2019-09-20 19:13:26 -0700

    Added on button click functions for each image. Implements user score and then checks if the player has won or lost yet. Win/loss condition is handled by a function called checkResult().

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,12 +23,52 @@ var resetGame = function() {
     purpleGemValue = randomGemValue();
     blueGemValue = randomGemValue();
     targetScore = randomTargetValue();
+    userScore = 0;
     $("#targetScore").text(targetScore);
     $("#userScore").text(userScore);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
     console.log("green: " + greenGemValue);
     console.log("red: " + redGemValue);
     console.log("blue: " + blueGemValue);
     console.log("purple: " + purpleGemValue);
 }
+
+var checkResult = function() {
+    if (targetScore === userScore) {
+        wins++;
+        alert("You Win!!");
+        resetGame();
+    }
+    else if(userScore > targetScore) {
+        losses++;
+        alert("You Lost!");
+        resetGame();
+    }
+}
+
+$("#blueGem").on("click", function() {
+    userScore += blueGemValue;
+    $("#userScore").text(userScore);
+    checkResult();
+});
+
+$("#redGem").on("click", function() {
+    userScore += redGemValue;
+    $("#userScore").text(userScore);
+    checkResult();
+});
+
+$("#greenGem").on("click", function() {
+    userScore += greenGemValue;
+    $("#userScore").text(userScore);
+    checkResult();
+});
+
+$("#purpleGem").on("click", function() {
+    userScore += purpleGemValue;
+    $("#userScore").text(userScore);
+    checkResult();
+});
 
 resetGame();
